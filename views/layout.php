ce7a640d1e77aee84b5a3d0d0cbcf4d68f7ffddb
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-04-30 23:39:28 -0600

    CreamosHedearAndFooter

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DevWebCamp - <?php echo $titulo; ?></title>
+    <title>EducaVet <?php echo $titulo; ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap" rel="stylesheet">
