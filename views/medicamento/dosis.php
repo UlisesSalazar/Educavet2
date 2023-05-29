a1e76b9d1cad01532085fe63e88d5f741207f0e2
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-05-29 16:49:55 -0600

    scssDosisIcono

--- views/medicamento/dosis.php
+++ views/medicamento/dosis.php
@@ -2,9 +2,12 @@
 <main class="dosis">
     <div class="tabla">
         <h2 class="dosis__heading"><?php echo $titulo; ?></h2>
-        <i class="fa-sharp fa-solid fa-syringe" style="color: #8d8600;"></i>
-
+     <div class ="tabla__icono">
+        <i class="fa-sharp fa-solid fa-syringe tabla__icono" style="color: #8d8600;"></i>
     </div>
+   
+
+</div>
 
 
 
