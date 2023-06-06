0d87421cd8a2356f94a827487ff3b5e9cca8fa5e
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-06-06 16:35:24 -0600

    DBMedicamentos

--- views/medicamento/dosis.php
+++ views/medicamento/dosis.php
@@ -7,9 +7,34 @@
     </div>
 </div>
 <div class= "dashboard__contenedor">
- <?php if(!empty($medicamentos)){ ?>
+ <?php if(!empty($medicamentos)) { ?>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col" class="table__th">Nombre</th>
+                <th scope="col" class="table__th">Dosis</th>
+                <th scope="col" class="table__th">Indicaciones</th>
+            </tr>
+            <tbody class="table__tbody">
+                <?php foreach($medicamentos as $medicamento) { ?>
+                    <tr class="table__tr">
+                        <td class="table__td">
+                            <?php echo $medicamento->nombre ?>
+                        </td>
+                        <td class="table__td">
+                            <?php echo $medicamento->dosis ?>
+                        </td>
+                        <td class="table__td">
+                            <?php echo $medicamento->indicaciones2 ?>
+                        </td>
 
+                    </tr>
+
+                <?php } ?>
+            </tbody>
+        </thead>
+    </table>
     <?php } else { ?>
 
         <p class="text-center"> No hay medicamentos aún</p>
