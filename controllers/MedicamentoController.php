38cbd9959b1bb6a2660fd0b829487c9f5a107714
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-06-15 12:56:57 -0600

    paginaActualRedireccionar

--- controllers/MedicamentoController.php
+++ controllers/MedicamentoController.php
@@ -10,13 +10,19 @@ class MedicamentoController {
     
     public static function dosis (Router $router) {
 
-        $pagina_actual = 1;
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+       if(!$pagina_actual || $pagina_actual < 1){
+           header('Location: /dosis?page=1');
+       }
+
         $registros_por_pagina = 10;
         $total = 15;
 
         $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
 
-        debuguear($paginacion);
+      
 
 
         $medicamentos = Medicamento::all();
