725ee5715f6c58a34eb30aa1c07668e7ef0206ab
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-06-15 11:26:16 -0600

    PaginacionDBMedicamentos

--- controllers/MedicamentoController.php
+++ controllers/MedicamentoController.php
@@ -2,12 +2,23 @@
 
 namespace Controllers;
 
-use Model\Medicamento;
 use MVC\Router;
+use Model\Medicamento;
+use Classes\Paginacion;
 
 class MedicamentoController {
     
     public static function dosis (Router $router) {
+
+        $pagina_actual = 1;
+        $registros_por_pagina = 10;
+        $total = 15;
+
+        $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
+        debuguear($paginacion);
+
+
         $medicamentos = Medicamento::all();
         
 
