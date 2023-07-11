2f5fd621b44b2ee700ce51eb3ce4c31c8f62ba21
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-07-11 13:42:56 -0600

    APIMedicamentos

---
+++ controllers/APIMedicamentos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+use Model\Medicamento;
+
+
+class APIMedicamentos {
+
+    public static function index() {
+
+     $medicamentos = Medicamento::all();
+     echo json_encode($medicamentos);
+    }
+
+
+
+}
