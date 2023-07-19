1e7e5a3408effb5a5caa4ff051526594837430f8
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-07-19 15:15:18 -0600

    functionPaginaActual

--- includes/funciones.php
+++ includes/funciones.php
@@ -10,3 +10,6 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+function pagina_actual($path) : bool {
+    return str_contains($_SERVER ['PATH_INFO'], $path ) ? true : false;
+}
