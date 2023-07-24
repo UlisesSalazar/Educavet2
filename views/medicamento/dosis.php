1b8d7ebf59fbfcc35abc3b6f7ac78d4cf4aeb58d
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-07-24 14:07:59 -0600

    animationIconDosis

--- views/medicamento/dosis.php
+++ views/medicamento/dosis.php
@@ -2,8 +2,8 @@
 <main class="dosis">
 
     <div class="tabla">
-        <h2 class="dosis__heading"><?php echo $titulo; ?></h2>
-     <div class ="tabla__icono">
+        <h2  class="dosis__heading"><?php echo $titulo; ?></h2>
+     <div  data-aos="fade-up-right" class ="tabla__icono">
         <i class="fa-sharp fa-solid fa-syringe tabla__icono" style="color: #00c7fc;"></i>
     </div>
 </div>
