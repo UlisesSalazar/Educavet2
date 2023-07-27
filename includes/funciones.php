bd87c7ba014558b93f5d04f9ef7181095922dd6b
karim salazar <42070125+UlisesSalazar@users.noreply.github.com>
2023-07-27 11:34:58 -0600

    functionAnimacion

--- includes/funciones.php
+++ includes/funciones.php
@@ -13,3 +13,10 @@ function s($html) : string {
 function pagina_actual($path) : bool {
    return str_contains( $_SERVER ['PATH_INFO']?? '/', $path ) ? true : false;
 }
+function aos_animacion() : vold {
+    $efectos = ['fade-up','fade-down', 'fade-right', 'fade-left', 'flip-left', 'zoom-in', 'zoom-in-up', 'zoom-out'];
+    $efecto = array_rand($fectos, 1);
+
+    echo ' data-aos"' . $efectos [$efecto] . '" ';
+
+}
