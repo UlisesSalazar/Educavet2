f3767b9944916429d5fc02db8081e7aae2a3426f
karim salazar <karim_salazar@outlook.com>
2023-08-31 14:01:16 -0700

    cambiosSubtitulos

--- views/paginas/radiografias.php
+++ views/paginas/radiografias.php
@@ -1,6 +1,6 @@
 <main class="radiografia">
     <h2 class="radiografias__heading"><?php echo $titulo; ?></h2>
-    <p class="radiografia__descripcion">Radiología clínica en caballos</p>
+    <p class="radiografia__descripcion">Radiología clínica del caballo</p>
     
     <div class="imagen">
     <h3 class="imagen__heading"> Tomas  (aqui ira imagenes) </h3>
@@ -9,7 +9,7 @@
         <div class="imagen__listado">
 
         </div>
-
+ 
 
     </div>
 
