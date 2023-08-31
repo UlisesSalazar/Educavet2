29f417f26df43f6ad796a05c16f97fbeb9050325
karim salazar <karim_salazar@outlook.com>
2023-08-31 12:46:41 -0700

    subtitulosRadio

--- views/paginas/radiografias.php
+++ views/paginas/radiografias.php
@@ -1,8 +1,11 @@
-<main class="radiografias">
+<main class="radiografia">
 <h2 class="radiografias__heading"><?php echo $titulo; ?></h2>
-<p>Radiología clinica en caballos</p>
+<p>Radiología clínica en caballos</p>
  
-
+<div class="imagen">
+ <h3 class="imagen__heading"> Tomas  (aqui ira imagenes) </h3>
+    <p class="imagen_fecha">Vistas Rodiológicas</p>
+</div>
 
 
 </main>
