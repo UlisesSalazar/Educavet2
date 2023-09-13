fba3a977ca0c8e705d2552bca334f488ed166d6d
karim salazar <karim_salazar@outlook.com>
2023-09-13 15:45:34 -0700

    ModelRadio

---
+++ models/Radiografia.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Model;
+
+class radiografia extends ActiveRecord {
+    protected static $tabla = 'radiografias';
+    protected static $columnasDB = ['id', 'nombre', 'apellido', 'imagen', 'tags'];
+    
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $imagen;
+    public $tags;
+
+    
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->imagen = $args['imagen'] ?? '';
+        $this->tags = $args['tags'] ?? '';
+      
+    }
+}
