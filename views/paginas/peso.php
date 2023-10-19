400cc58d8d51162dc379e25e740d75c10bb1f2f8
karim salazar <karim_salazar@outlook.com>
2023-10-19 16:56:01 -0700

    imagenEnPesoCaballo

--- views/paginas/peso.php
+++ views/paginas/peso.php
@@ -1,4 +1,17 @@
 <main class="peso">
-<h2 class="peso__heading"><?php echo $titulo ?></h2>
+    <h2 class="peso__heading"><?php echo $titulo ?></h2>
+    <p class="peso__descripcion"> Si no cuentas con una báscula, calcula el peso del caballo acontinuación</p>
+
+    <div class="peso__grid">
+        <div class="peso__imagen">
+            <picture>
+                <source srcset="build/img/caballoPerimetro.png" type="image/png">
+                <img loading="lazy" width="30" height="30" alt="imgagen caballo">
+          
+            </picture>
+
+        </div>
+
+    </div>
 
 </main>
