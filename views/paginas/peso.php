ebf31375ceff96f16dab96425521afa739d20998
karim salazar <karim_salazar@outlook.com>
2023-10-28 23:44:19 -0700

    cambiosAPesoCaballo

--- views/paginas/peso.php
+++ views/paginas/peso.php
@@ -1,19 +1,19 @@
 <main class="peso">
     <h2 class="peso__heading"><?php echo $titulo ?></h2>
-    <p class="peso__descripcion"> Nota: El peso no es exacto y puede variar un poco</p>
-    <p class="peso__descripcion"> Si no cuentas con una báscula, calcula el peso del caballo acontinuación  </p>
+    <p  class="peso__descripcion"> Nota: El peso no es exacto y puede variar un poco</p>
+    <p  class="peso__descripcion" > Si no cuentas con una báscula, calcula el peso del caballo o potro acontinuación:  </p>
 
     <div class="peso__grid">
         <div class="peso__imagen">
             <picture>
                 <source srcset="build/img/caballoPerimetro.png" type="image/png">
-                <img loading="lazy" width="30" height="30" alt="imagen caballo">
+                <img data-aos="fade-left" loading="lazy" width="30" height="30" alt="imagen caballo">
           
             </picture>
         </div>
     <div class="peso__contenido">
         <!-- the data will go here -->
-        <p class="peso__texto">Ingresa aquí el dato de Perimetro Torácico</p>
+        <p class="peso__texto">Ingresa el dato de Perimetro Torácico</p>
     
         <form class="peso__calculo">
             <fieldset>
@@ -22,7 +22,7 @@
                     <option value="2">Potro</option>
                 </select>
                 <label for="Titulo">P. Torácico (metros):</label>
-                <input type="number" id="perimetro" placeholder=" P.T. Ej: 2.5, 0.5 " min="0">
+                <input type="number" id="perimetro" placeholder=" P.T.-Ej: 2.5, 0.5 " min="0">
                 <input type="submit" value="Calcular peso" class="peso__boton">
             </fieldset>
         </form>
