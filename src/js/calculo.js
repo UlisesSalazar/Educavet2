e40738e02de8de88bb19cd02a8998935a3128028
karim salazar <karim_salazar@outlook.com>
2023-10-30 23:06:41 -0700

    JS-calculoPrimerosPasos

--- src/js/calculo.js
+++ src/js/calculo.js
@@ -1,1 +1,23 @@
-console.log('hola mundo');
+ (function calculo(){
+
+    const calculoInput = document.querySelector('#calcularPeso')
+     
+
+     if(calculoInput){
+        
+        const peso = Number(document.getElementById('valor1').value);
+        const exponente = 3;
+        let r = 1;
+
+       for(let i = 0; i<exponente; i++){
+        resultado = r * peso;
+        }
+       
+       alert(resultado);
+    }
+   
+
+
+ })()
+
+
--- views/paginas/peso.php
+++ views/paginas/peso.php
@@ -8,27 +8,25 @@
             <picture>
                 <source srcset="build/img/caballoPerimetro.png" type="image/png">
                 <img data-aos="fade-left" loading="lazy" width="30" height="30" alt="imagen caballo">
-          
             </picture>
         </div>
-    <div class="peso__contenido">
-        <!-- the data will go here -->
-        <p class="peso__texto">Ingresa el dato de Perimetro Torácico</p>
-    
-        <form class="peso__calculo">
-            <fieldset>
-                <select class="peso__opcion">
-                    <option value="1">Caballo</option>
-                    <option value="2">Potro</option>
-                </select>
-                <label for="Titulo">P. Torácico (metros):</label>
-                <input type="number" id="perimetro" placeholder=" P.T.-Ej: 2.5, 0.5 " min="0">
-                <input type="submit" value="Calcular peso" class="peso__boton">
-            </fieldset>
-        </form>
 
-    </div>
+        <div class="peso__contenido">
+            <!-- the data will go here -->
+            <p class="peso__texto">Ingresa el dato de Perimetro Torácico</p>
+            <form class="peso__calculo">
+                <fieldset>
+                    <select class="peso__opcion">
+                        <option id="filtro" value="90">Caballo</option>
+                        <option  id="filtro"  value="80">Potro</option>
+                    </select>
+                    <label for="Titulo">P. Torácico (metros):</label>
+                    <input type="number" id="valor1" placeholder=" P.T.-Ej: 2.5, 0.5 " min="0">
+                    <input type="submit" id="calcularPeso" value="Calcular" class="peso__boton">
+                </fieldset>
+            </form>
+        </div>
 
-    </div>
+ </div>
 
 </main>
