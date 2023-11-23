b7d6bd3e1b620654c71f3ed61ef4e919780393ec
karim salazar <karim_salazar@outlook.com>
2023-11-22 16:13:54 -0800

    cambioARadioButtonAndSCss

--- src/js/calculo.js
+++ src/js/calculo.js
@@ -3,8 +3,17 @@
 (function(){
       const valor1 = document.querySelector("#valor1")
 
-      if( valor1){
+      if(valor1){
 
-      }
+         //listen to what th write
+         valor1.addEventListener('keypress', guardarValor)
+
+      function guardarValor(e) {
+         
+         // console.log(e.keyCode)
+         console.log(valor1)
+
+       }
+    }
 
 })();
--- views/paginas/peso.php
+++ views/paginas/peso.php
@@ -13,13 +13,19 @@
 
         <div  id="peso" class="peso__contenido">
             <!-- the data will go here -->
-            <p class="peso__texto">Ingresa el dato de Perimetro Torácico</p>
+            <p class="peso__texto">Ingresa el dato de Perímetro Torácico</p>
             <form  id="peso" class="peso__calculo">
                 <fieldset>
-                    <select class="peso__opcion">
+                <div class="peso__opcion">
+                    <input type="radio" id="valor2" name="caballo" />
+                    <label for="valor2">Caballo</label>
+                    <input type="radio" id="valor3" name="potro" />
+                    <label for="valor2">Potro</label>
+                </div>
+                    <!-- <select class="peso__opcion">
                         <option id="filtroCaballo" value="caballo" name="total[caballo]">Caballo90</option>
                         <option  id="filtroPotro"  value="potro" name="total[caballo]">Potro80</option>
-                    </select>
+                    </select> -->
                     <label for="Titulo">P. Torácico (metros):</label>
                     <input 
                     type="text"
@@ -27,8 +33,8 @@
                      name="valor1" 
                      placeholder=" P.T.-Ej: 2.5, 0.5 "
                      >
-                     
-                    <input type="submit" id="calcularPeso" onclick="calculo()" class="peso__boton">
+
+                    <!-- <input type="submit" id="calcularPeso" onclick="calculo()" class="peso__boton"> -->
                 
                     <div class="total"></div>
                 </fieldset>
